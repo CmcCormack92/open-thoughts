6ed4fa4394d92af12ea508d4bcb76659f2f48635
CmcCormack92 <86354138+CmcCormack92@users.noreply.github.com>
2022-01-30 09:32:09 -0500

    initial commit

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -22,7 +22,7 @@ router.get('/:id', (req, res) => {
     include: [
       {
         model: Post,
-        attributes: ['id', 'title', 'post_url', 'created_at']
+        attributes: ['id', 'title', 'post_content', 'created_at']
       },
       {
         model: Comment,
@@ -108,7 +108,6 @@ router.post('/logout', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
